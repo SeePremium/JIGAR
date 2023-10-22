5776ffbb0cf4f8c247694d81600cee54862a4c4e
SeePremium <145249163+SeePremium@users.noreply.github.com>
2023-10-22 09:54:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'jnglink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '93afdd16e98ac2ba9461051503e759f4da683333')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
